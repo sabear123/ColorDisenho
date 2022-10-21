0263c4877aa591073dafb8d0997cf86dae4bc75e
SALOMON BEDOYA ARISTIZABAL <salomonbedoya301441@correo.itm.edu.co>
2022-10-21 15:15:15 -0500

    Parte 1 Vistas 1

---
+++ ColorDisenho/ColorDisenho/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using ColorDisenho.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ColorDisenho.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult MenuAdmin()
+        {
+            return View();
+        }
+
+        public IActionResult CrearEmpleado()
+        {
+            return View();
+        }
+
+        public IActionResult ActualizarProduccion()
+        {
+            return View();
+        }
+
+        public IActionResult Insumos()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ColorDisenho/ColorDisenho/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ColorDisenho.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
